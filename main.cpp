3c484a2458f00d09631baf0c6f4f9755ae1e3d3f
vedaRadev <ryancarmstrong97@gmail.com>
2025-03-24 19:13:49 -0400

    ensure arena alloc aligns are powers of two, update tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
 #include <cstdio>
 
+inline bool is_power_of_two(uint64_t x) { return ~(x & (x - 1)); }
+
+//============================== ARENA ==============================//
 struct Arena {
     void* m_memory;
     size_t m_offset;
@@ -12,7 +16,8 @@ struct Arena {
         if (bytes == 0) return nullptr;
 
         uintptr_t base_addr = (uintptr_t)m_memory + m_offset;
-        size_t padding = (align - (base_addr % align)) % align;
+        assert(is_power_of_two(align));
+        size_t padding = align - base_addr & (align - 1);
         if (m_offset + bytes + padding > m_capacity) { return nullptr; }
 
         m_offset += bytes + padding;
@@ -21,26 +26,57 @@ struct Arena {
 
     void reset() { m_offset = 0; }
 };
+///////////////////////////////////////////////////////////////////////
+//============================== TESTS ==============================//
+///////////////////////////////////////////////////////////////////////
 
-int main() {
-    const int arena_size = 1 << 8;
+// Test results
+struct TestResult {
+    bool passed;
+    int failure_line;
+    const char* failure_expr;
+};
+
+#define TEST TestResult
+
+#define RUN_TEST(label, test) { \
+    printf("test: %s... ", label); \
+    TestResult result = test(); \
+    if (result.passed) { \
+        printf("ok\n"); \
+    } else { \
+        printf("failed (line %d): %s\n", result.failure_line, result.failure_expr); \
+    } \
+};
+
+#define TEST_ASSERT(expr) if (!(expr)) { \
+    return TestResult { \
+        .passed = false, \
+        .failure_line = __LINE__, \
+        .failure_expr = #expr \
+    }; \
+}
+
+#define TEST_END return { .passed = true };
+
+TEST test_arena() {
+    const size_t arena_size = 8;
     Arena arena = { .m_memory = malloc(arena_size), .m_capacity = arena_size };
 
-    void* result;
+    TEST_ASSERT(arena.alloc_aligned(4, 4) != nullptr);
+    TEST_ASSERT(arena.alloc_aligned(1, 1) != nullptr);
+    TEST_ASSERT(arena.alloc_aligned(4, 4) == nullptr);
+    arena.reset();
+    TEST_ASSERT(arena.alloc_aligned(4, 1) != nullptr);
+    TEST_ASSERT(arena.alloc_aligned(4, 16) == nullptr);
 
-    result = arena.alloc_aligned(250, 8);
-    if (result == nullptr) {
-        printf("First allocation failed but shouldn't have\n");
-        return 1;
-    }
+    TEST_END
+}
 
-    result = arena.alloc_aligned(8, 4);
-    if (result != nullptr) {
-        printf("Second allocation didn't fail but should have\n");
-        return 1;
-    }
-
-    printf("success");
-
+///////////////////////////////////////////////////////////////////////////
+//============================== END TESTS ==============================//
+///////////////////////////////////////////////////////////////////////////
+int main() {
+    RUN_TEST("arena", test_arena);
     return 0;
 }
